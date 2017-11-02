c031ffa11c14888d53b1b17a6eeb3da69aec2876
“Shulin <ernestwoods0516@gmail.com>
2017-11-01 20:37:13 -0400

    address one

---
+++ TestBlueAtt/app/src/main/java/com/example/testblueatt/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.testblueatt;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    private final static int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter mBluetoothAdapter;
+    Button b1,b2,b3,b4;
+    private Set<BluetoothDevice>pairedDevices;
+    ListView lv;
+    ListView lv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        b1 = (Button) findViewById(R.id.on);
+        b2=(Button)findViewById(R.id.visible);
+        b3=(Button)findViewById(R.id.ld);
+        b4=(Button)findViewById(R.id.off);
+        b4=(Button)findViewById(R.id.scan);
+        lv = (ListView)findViewById(R.id.listView);
+        lv2 = (ListView)findViewById(R.id.listView2);
+
+        /*
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, filter);
+        */
+    }
+
+    public void scan(View v) {
+
+
+
+    }
+
+
+    public void on(View v) {
+        {
+
+
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    public void off(View v){
+        mBluetoothAdapter.disable();
+        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
+    }
+
+    public  void visible(View v){
+        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        startActivityForResult(getVisible, 0);
+    }
+
+    public void list(View v){
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        ArrayList address = new ArrayList();
+
+        for(BluetoothDevice bt : pairedDevices) address.add(bt.getAddress());
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
+
+        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, address);
+
+        lv.setAdapter(adapter);
+    }
+
+    public void listName(View v){
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        ArrayList name = new ArrayList();
+
+        for(BluetoothDevice bt : pairedDevices) name.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
+
+        final ArrayAdapter adapter2 = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, name);
+        lv2.setAdapter(adapter2);
+    }
+
+
+
+
+}
